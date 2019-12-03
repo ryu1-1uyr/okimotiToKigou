093c53990fc3194364b3b2c558f12745d4fe673a
s17001 <s17001@std.it-college.ac.jp>
2019-12-04 01:48:11 +0900

    ツイートするためにエンコーダーしたり文字切り替えたりする奴の実装

--- src/components/tweetButton.vue
+++ src/components/tweetButton.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    ついーとするためのボタン
+    <a class="tweet" :href="'https://twitter.com/intent/tweet?text='+twitter+'&hashtags=kuso_app_2019'" target="_blank">ついーとするためのボタン</a>
   </div>
 </template>
 
 <script>
   export default {
-    name: "tweetButton"
+    name: "tweetButton",
+    props:{
+      tweetText: String
+    },
+    computed:{
+      twitter(){
+        console.log(encodeURI(this.tweetText).split('+').join("%2B"))
+        return encodeURI(this.tweetText).split('+').join("%2B")
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  .tweet{
+    background: #3bb4d0;
+  }
 </style>
